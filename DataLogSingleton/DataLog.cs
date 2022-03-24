cfc46b9e82a08ff1ce136b5578a40e5cd719f7a1
carlospcneto <carlospcneto@hotmail.com>
2022-03-24 00:47:24 -0300

    Commit com Desafio técnico da Spectral

---
+++ DataLogSingleton/DataLog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DataLogSingleton
+{
+    public sealed class DataLog
+    {
+        private static DataLog? _instance;
+        public List<string> logs;
+        private static readonly object _lock = new object();
+        private DataLog()
+        {
+            logs = new List<string>();
+        }
+        public static DataLog GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock(_lock)
+                {
+                    _instance = new DataLog();                    
+                }
+            }
+            return _instance;
+        }
+
+
+        public void GravaLog(string mensagem) 
+        {
+            logs.Add(mensagem);
+        }
+
+        public void ApagaLogs()
+        {
+            logs.Clear();
+        }
+
+        public void ImprimeLogs()
+        {
+            foreach (string mensagem in logs)
+            {
+                Console.WriteLine(mensagem);
+            }
+        }
+    }
+}
---
+++ DataLogSingleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataLogSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataLog dataLog1 = DataLog.GetInstance();
+            DataLog dataLog2 = DataLog.GetInstance();
+
+            if (dataLog1 == dataLog2)
+            {
+                dataLog1.GravaLog("Both DataLog contain the same instance.");
+            }
+            else
+            {
+                dataLog1.GravaLog("DataLog contain differente instances.");
+            }
+
+            dataLog1.ImprimeLogs();
+            dataLog2.ApagaLogs();
+
+            dataLog2.GravaLog("Desenvolvido por Carlos Castro");
+            dataLog2.ImprimeLogs();
+        }
+    }
+}
